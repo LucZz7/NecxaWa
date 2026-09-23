5cab35993ba6e132da7c3869c8ca93b4a87bd8f0
NECXY <shantanujagadale018@gmail.com>
2026-09-23 12:25:33 +0530

    console ?api= autofill for 1-click cloud backend

--- js/config.js
+++ js/config.js
@@ -1,6 +1,14 @@
-/* NecxaWA config - backend connection stored locally in the browser */
+/* NecxaWA config - backend connection stored locally in the browser.
+   Tip: console.html?api=https://xxx-2785.app.github.dev pre-fills the URL. */
 (function () {
   const DEFAULT_BASE = "http://localhost:2785";
+  // ?api= query param (e.g. from the 1-click cloud backend link) pre-fills + saves the URL
+  try {
+    const q = new URLSearchParams(location.search).get("api");
+    if (q && /^https?:\/\//i.test(q)) {
+      localStorage.setItem("necxawa_api_base", q.replace(/\/+$/, ""));
+    }
+  } catch (e) {}
   window.NECXAWA = {
     getApiBase() {
       return (localStorage.getItem("necxawa_api_base") || DEFAULT_BASE).replace(/\/+$/, "");
